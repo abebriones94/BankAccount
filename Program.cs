1a823ae072c56e0ac110f78fc831ceee71cbafc3
abebr <abebr@ABEBRIONES>
2022-10-28 21:52:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using MySuperBank;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Calling the constructor method 
+        var account = new BankAccount("Kendra", 10000);
+
+        Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}$");
+
+        account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+        Console.WriteLine($"{account.Balance}$ is your new balance");
+
+        account.MakeWithdrawal(50, DateTime.Now, "Xbox Game");
+        Console.WriteLine($"{ account.Balance}$");
+
+        Console.WriteLine();
+
+        Console.WriteLine(account.GetAccountHistory());
+
+
+        // CTRL C + K will comment out multiple lines of code 
+
+        //// Tests for a negative balance
+        //try
+        //{
+        //    account.MakeWithdrawal(75000, DateTime.Now, "Attempt to overdraw");
+        //}
+        //catch (InvalidOperationException e)
+        //{
+        //    Console.WriteLine("Exception caught trying to overdraw");
+        //    Console.WriteLine(e.ToString());
+        //}
+
+        //// Tests that the initial balance must be positive 
+        //try
+        //{
+        //    var invalidAccount = new BankAccount("invalid", -55);
+        //}
+        //catch (ArgumentOutOfRangeException e)
+        //{
+        //    Console.WriteLine("Exception caught creating account with negative balance");
+
+
+        }
+    }
---
+++ Transaction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySuperBank
+{
+    internal class Transaction
+    {
+        // prop prop = properties 
+
+        public decimal Amount { get; }
+
+        public DateTime Date { get; }
+
+        public string Notes { get; }
+
+        // CONSTRUCTOR to MAKE Transactions 
+
+        public Transaction(decimal account, DateTime date, string note)
+        {
+            Amount = account;
+            Date = date;
+            Notes = note;
+        }
+
+    }
+}
