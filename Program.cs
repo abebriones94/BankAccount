6a405a309d1e0ad9134f71a011d3dc88fd3c3d65
Abe Briones <abebriones94@gmail.com>
2022-10-30 15:35:43 +0100

    Added unit testing and a new library

--- Program.cs
+++ Program.cs
@@ -1,47 +1,31 @@
-﻿using MySuperBank;
+﻿using BankLibrary;
+using System;
 
-internal class Program
+
+internal class MySuperBank
 {
     private static void Main(string[] args)
+
+
     {
+        // CTRL F5 to run without debug 
+
         // Calling the constructor method 
         var account = new BankAccount("Kendra", 10000);
 
         Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}$");
 
         account.MakeWithdrawal(120, DateTime.Now, "Hammock");
-        Console.WriteLine($"{account.Balance}$ is your new balance");
-
         account.MakeWithdrawal(50, DateTime.Now, "Xbox Game");
-        Console.WriteLine($"{ account.Balance}$");
+        account.MakeWithdrawal(5, DateTime.Now, "Coffee");
+        account.MakeWithdrawal(5, DateTime.Now, "Diet Coke");
+        account.MakeWithdrawal(7, DateTime.Now, "Tea");
+        account.MakeWithdrawal(8, DateTime.Now, "Pants");
 
-        Console.WriteLine();
 
         Console.WriteLine(account.GetAccountHistory());
 
 
-        // CTRL C + K will comment out multiple lines of code 
 
-        //// Tests for a negative balance
-        //try
-        //{
-        //    account.MakeWithdrawal(75000, DateTime.Now, "Attempt to overdraw");
-        //}
-        //catch (InvalidOperationException e)
-        //{
-        //    Console.WriteLine("Exception caught trying to overdraw");
-        //    Console.WriteLine(e.ToString());
-        //}
-
-        //// Tests that the initial balance must be positive 
-        //try
-        //{
-        //    var invalidAccount = new BankAccount("invalid", -55);
-        //}
-        //catch (ArgumentOutOfRangeException e)
-        //{
-        //    Console.WriteLine("Exception caught creating account with negative balance");
-
-
-        }
     }
+}
